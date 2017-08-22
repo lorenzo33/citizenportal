a0edfcd90a935fed42ae96a15b958f981e0b5892
DELAPLACE Laurent <laurent.delaplace@u-bordeaux.fr>
2017-08-22 16:24:44 +0200

    ajout adresse ip table borne

--- gestion/models.py
+++ gestion/models.py
@@ -74,7 +74,7 @@ class Bornes(models.Model):
             
     nom = models.CharField(max_length=100)
     lieu = models.CharField(max_length=100)
-    adresse_ip = models.CharField(max_length=20)		
+    adresse_ip = models.GenericIPAddressField(protocol='IPv4')		
     etat = models.IntegerField(choices=STATUT_BORNE, default=1)
     service = models.ForeignKey(Service)
     
