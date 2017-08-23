abd38367fdbfc90ed412e930141766b00730206e
DELAPLACE Laurent <laurent.delaplace@u-bordeaux.fr>
2017-08-23 14:08:33 +0200

    ajout des services porteurs

--- gestion/adminx.py
+++ gestion/adminx.py
@@ -4,10 +4,32 @@ from xadmin.layout import Tab, TabHolder, Fieldset, PrependedText, Row
 from xadmin.plugins.inline import Inline
 from easy_select2 import select2_modelform
 
-from gestion.models import Porteur, Carte, Service, Bornes
+from gestion.models import Porteur, Carte, Service, Bornes, SouscriptionSvc
+
+
+class SouscriptionSvcInLine(object):
+    model = SouscriptionSvc
+    style = 'table'
+    extra = 3
 
 class PorteurAdmin(object):
+    
+    list_display = ('nom','prenom','statut')
+    search_fields = ('nom',)
+    list_filter = ('statut',)
     model_icon = 'fa fa-user'
+    
+    inlines = (SouscriptionSvcInLine,)
+    
+    form_layout = (
+        TabHolder(
+            Tab('Général',
+                Fieldset('Généralités','nom','prenom','date_de_naissance','statut'),                         
+                Inline(SouscriptionSvc),
+            ),
+                              
+        ),                      
+    )
     
 class CarteAdmin(object):
     model_icon = 'fa fa-address-card-o '
