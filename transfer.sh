be18cd6cdba4324baf7ae52bfc9b313163d17136
DELAPLACE Laurent <laurent.delaplace@u-bordeaux.fr>
2017-09-13 15:50:02 +0200

    modification script export

--- transfer.sh
+++ transfer.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 cd /home/django/workspace/citizenportal/
-echo "Répertoire courant "$pwd >> tonfichierlog.log 
+echo "Répertoire courant "$pwd >> export_base.log 
 python manage.py dumpdata gestion.carte > carte.json
 
-# si tu veux tester ton serveur depuis client ou l'inverse
-ping -c 1 -w 4 192.168.70.104 > /dev/null
+# On teste la connexion a la borne et on envoie le fichiere
+ping -c 1 -w 4 192.168.70.107 > /dev/null
 EXTLINK=$?
 if [ $EXTLINK -ne 0 ]
 then
-	echo "$(date) : no VPN link ..." >> tonfichierlog.log
+	echo "$(date) : no VPN link ..." >> export_base.log
 	exit 111
 fi
 
 
 # scp
-scp /home/django/workspace/citizenportal/carte.json pi@192.168.70.104:/home/pi/citizenpass 2>> tonfichierlog.log
+scp /home/django/workspace/citizenportal/carte.json pi@192.168.70.107:/home/pi/citizenpass 2>> export_base.log
